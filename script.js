c98cff1164536210274cd3ce2bd13162644ffcd8
pragatiagrawal69 <your.email@example.com>
2025-12-03 21:08:05 +0530

    class add(shakeHand)

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 const scissorsBtn = document.querySelector('.scissors-btn')
 const rockBtn = document.querySelector('.rock-btn')
-const papperBtn = document.querySelector('.paper-btn')
+const paperBtn = document.querySelector('.paper-btn')
+const userHandIcon = document.querySelector('.user.hand-icon')
+const computerHandIcon = document.querySelector('.computer.hand-icon')
+
+scissorsBtn.addEventListener('click',()=>{
+      userHandIcon.classList.add('shakeUserHands')
+    computerHandIcon.classList.add('shakeComputerHands')
+
+    setTimeout(() => {
+        userHandIcon.classList.remove('shakeUserHands')
+    computerHandIcon.classList.remove('shakeComputerHands')
+    }, 500);
+})
+rockBtn.addEventListener('click',()=>{
+    userHandIcon.classList.add('shakeUserHands')
+    computerHandIcon.classList.add('shakeComputerHands')
+
+    setTimeout(() => {
+        userHandIcon.classList.remove('shakeUserHands')
+    computerHandIcon.classList.remove('shakeComputerHands')
+    }, 500);
+})
+paperBtn.addEventListener('click',()=>{
+     userHandIcon.classList.add('shakeUserHands')
+    computerHandIcon.classList.add('shakeComputerHands')
+
+    setTimeout(() => {
+        userHandIcon.classList.remove('shakeUserHands')
+    computerHandIcon.classList.remove('shakeComputerHands')
+    }, 500);
+})
