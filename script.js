3c51fc31afb69520da7c20f7826b80f64dc37903
pragatiagrawal69 <your.email@example.com>
2025-12-05 11:09:54 +0530

    functioning calculatedResult

--- script.js
+++ script.js
@@ -4,74 +4,45 @@ const paperBtn = document.querySelector('.paper-btn')
 const userHandIcon = document.querySelector('.user.hand-icon')
 const computerHandIcon = document.querySelector('.computer.hand-icon')
 const result = document.querySelector('.result')
+const userScore = document.querySelector('.user-score')
+const computerScore = document.querySelector('.computer-score')
+
 const rockIcon = '👊'
 const paperIcon = '🖐️'
 const scissorIcon = '✌️'
 const iconList = [rockIcon,paperIcon,scissorIcon]
-scissorsBtn.addEventListener('click',()=>{
-    result.innerText = ''
-    userHandIcon.innerText = '🤜'
-    computerHandIcon.innerText = '🤛'
-      userHandIcon.classList.add('shakeUserHands')
-    computerHandIcon.classList.add('shakeComputerHands')
-    setTimeout(() => {
-        userHandIcon.classList.remove('shakeUserHands')
-    computerHandIcon.classList.remove('shakeComputerHands')
-    userHandIcon.innerText = scissorIcon
-        const computerChoice = Math.floor(Math.random() *3)
-     computerHandIcon.innerText = iconList[computerChoice]
-     if(userHandIcon.innerText===computerHandIcon.innerText){
-        result.innerText = 'draw'
-     } else if(computerHandIcon.innerText == paperIcon){
-        result.innerText= 'You won!!'
-     } else{
-        result.innerText = 'Computer won!!'
-     }
-    }, 500);
-})
-rockBtn.addEventListener('click',()=>{
-      result.innerText = ''
+
+function calculateResult(selectedIcon, winningIcon){
+     result.innerText = ''
     userHandIcon.innerText = '🤜'
     computerHandIcon.innerText = '🤛'
     userHandIcon.classList.add('shakeUserHands')
     computerHandIcon.classList.add('shakeComputerHands')
-
     setTimeout(() => {
         userHandIcon.classList.remove('shakeUserHands')
     computerHandIcon.classList.remove('shakeComputerHands')
-    userHandIcon.innerText= rockIcon
+    userHandIcon.innerText= selectedIcon
     const computerChoice = Math.floor(Math.random() *3)
      computerHandIcon.innerText = iconList[computerChoice]
      if(userHandIcon.innerText===computerHandIcon.innerText){
         result.innerText = 'draw'
-     } else if(computerHandIcon.innerText === scissorIcon){
+     } else if(computerHandIcon.innerText === winningIcon){
         result.innerText= 'You won!!'
+        userScore.innerText =  +userScore.innerText + 1
      } else{
         result.innerText = 'Computer won!!'
+        computerScore.innerText = +computerScore.innerText + 1
      }
 }, 500);
+}
 
-    
+scissorsBtn.addEventListener('click',()=>{
+   calculateResult(scissorIcon, paperIcon)
+  
+})
+rockBtn.addEventListener('click',()=>{
+     calculateResult(rockIcon,scissorIcon)    
 })
 paperBtn.addEventListener('click',()=>{
-      result.innerText = ''
-    userHandIcon.innerText = '🤜'
-    computerHandIcon.innerText = '🤛'
-     userHandIcon.classList.add('shakeUserHands')
-    computerHandIcon.classList.add('shakeComputerHands')
-
-    setTimeout(() => {
-        userHandIcon.classList.remove('shakeUserHands')
-    computerHandIcon.classList.remove('shakeComputerHands')
-    userHandIcon.innerText= paperIcon
-        const computerChoice = Math.floor(Math.random() *3)
-     computerHandIcon.innerText = iconList[computerChoice]
-     if(userHandIcon.innerText===computerHandIcon.innerText){
-        result.innerText = 'draw'
-     } else if(computerHandIcon.innerText == rockIcon){
-        result.innerText= 'You won!!'
-     } else{
-        result.innerText = 'Computer won!!'
-     }
-    }, 500);
+   calculateResult(paperIcon, rockIcon)
 })
